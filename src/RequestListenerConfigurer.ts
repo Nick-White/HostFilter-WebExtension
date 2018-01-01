8cbee27171559a077f386bfe59a46c140a77d431
Nicolae Albu <nicolae_albu2004@yahoo.com>
2018-01-01 04:13:05 +0200

    #9 Redirect blocked requests to empty response according to type

--- src/RequestListenerConfigurer.ts
+++ src/RequestListenerConfigurer.ts
@@ -2,6 +2,7 @@ import { Hosts } from "./model/Hosts";
 import { Set } from "typescript-collections";
 import { CollectionUtils } from "./utils/CollectionUtils";
 import BlockingResponse = browser.webRequest.BlockingResponse;
+import ResourceType = browser.webRequest.ResourceType;
 import Tab = browser.tabs.Tab;
 import { Configuration } from "./model/Configuration";
 import { Log } from "./Log";
@@ -9,9 +10,10 @@ import { FilterOutcome } from "./model/FilterOutcome";
 
 export class RequestListenerConfigurer {
 
-    private static readonly BLOCKING_RESPONSE = {
-        redirectUrl: browser.extension.getURL("empty_content/text.txt")
+    private static readonly EMPTY_RESPONSE_URL_BY_RESOURCE_TYPE_MAP: {[resourceType: string]: string} = {
+        image: browser.extension.getURL("empty_content/image.png")
     };
+    private static readonly DEFAULT_EMPTY_RESPONSE_URL: string = browser.extension.getURL("empty_content/text.txt");
     
     private readonly blockedHosts: Set<string>;
 
@@ -23,18 +25,26 @@ export class RequestListenerConfigurer {
 
     public configure(): void {
         let log: Log = Log.getInstance();
-        browser.webRequest.onBeforeRequest.addListener((details: {tabId: number, url: string}): Promise<BlockingResponse> => {
+        browser.webRequest.onBeforeRequest.addListener((details: {tabId: number, url: string, type: ResourceType}): Promise<BlockingResponse> => {
             return new Promise<BlockingResponse>((resolve: (response: BlockingResponse) => void) => {
                 let url: string = details.url;
                 let host: string = this.getHost(url);
                 if (this.isBlockedHost(host)) {
                     log.log(url, host, FilterOutcome.BLOCKED);
-                    browser.tabs.get(details.tabId).then((tab: Tab) => {
-                        if (this.isFreshPopup(tab)) {
-                            browser.tabs.remove(details.tabId);
-                        }
-                        resolve(RequestListenerConfigurer.BLOCKING_RESPONSE);
-                    });
+                    if (details.type === "main_frame") {
+                        browser.tabs.get(details.tabId).then((tab: Tab) => {
+                            let redirectUrl: string;
+                            if (this.isFreshPopup(tab)) {
+                                browser.tabs.remove(details.tabId);
+                                redirectUrl = this.getRedirectUrl(details);
+                            } else {
+                                redirectUrl = <string>tab.url;
+                            }
+                            resolve({redirectUrl: redirectUrl});
+                        });
+                    } else {
+                        resolve({redirectUrl: this.getRedirectUrl(details)});
+                    }
                 } else {
                     log.log(url, host, FilterOutcome.ALLOWED);
                     resolve({});
@@ -43,6 +53,14 @@ export class RequestListenerConfigurer {
         }, {
             urls: ["<all_urls>"]
         }, ["blocking"]);
+    }
+
+    private getRedirectUrl(details: {url: string, type: ResourceType}): string {
+        let redirectUrl: string = RequestListenerConfigurer.EMPTY_RESPONSE_URL_BY_RESOURCE_TYPE_MAP[details.type];
+        if (redirectUrl === undefined) {
+            redirectUrl = RequestListenerConfigurer.DEFAULT_EMPTY_RESPONSE_URL;
+        }
+        return redirectUrl;
     }
 
     private isBlockedHost(host: string): boolean {
